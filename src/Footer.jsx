c1a133131e065ee7373d880d671279f01ca5ad9c
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-08-31 18:27:57 +0300

    added componets and their styles

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; All Rights reserved. </small>
+      <small>Last check limits: 17 days ago</small>
+    </footer>
+  );
+};
