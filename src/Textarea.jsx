8ff3d11496b551d944ca31aaf3c91d930ad31fbc
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-08-31 20:26:56 +0300

    added basic validation

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function Textarea() {
     const [text, setText] = useState('');
+    const [showWarning, setShowWarning] = useState(false);
+
+    const handleChange = (e) => {
+        let newText = e.target.value;
+
+        if (newText.includes('<script>')) {
+            setShowWarning(true);
+            newText = newText.replace('<script>', '');
+        }
+        setText(newText);
+    }
     return (
-        <textarea value={text} onChange={(e) => {
-            const newText = e.target.value;
-            setText(newText);
-        }} className="textarea" placeholder="Enter your text here..." spellCheck="false" />
+        <div className="textarea">
+            <textarea value={text} onChange={handleChange} placeholder="Enter your text here..." spellCheck="false" />
+            {
+                showWarning ? <Warning /> : null
+            }
+        </div>
     );
 };
