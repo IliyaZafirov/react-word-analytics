e528d99a0f0aff54c8b6f6aa896a915b06ca6c77
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-01 05:11:23 +0300

    added validations for php and html tags

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -4,13 +4,23 @@ import Warning from "./Warning";
 export default function Textarea() {
     const [text, setText] = useState('');
     const [showWarning, setShowWarning] = useState(false);
+    const [warningText, setWarningText] = useState('');
 
     const handleChange = (e) => {
         let newText = e.target.value;
 
         if (newText.includes('<script>')) {
+            setWarningText('No script tag allowed!')
             setShowWarning(true);
             newText = newText.replace('<script>', '');
+        } else if (newText.includes('<?php')) {
+            setWarningText('No php tag allowed!')
+            setShowWarning(true);
+            newText = newText.replace('<?php', '');
+        } else if (newText.includes('<html>')) {
+            setWarningText('No html tag allowed!')
+            setShowWarning(true);
+            newText = newText.replace('<html>', '');
         }
         setText(newText);
     }
@@ -18,7 +28,7 @@ export default function Textarea() {
         <div className="textarea">
             <textarea value={text} onChange={handleChange} placeholder="Enter your text here..." spellCheck="false" />
             {
-                showWarning ? <Warning /> : null
+                showWarning ? <Warning warningText={warningText} /> : null
             }
         </div>
     );
