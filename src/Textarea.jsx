90888d0310b0c6fa4f28a7994332c99bbfebb700
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-01 05:17:20 +0300

    removed boolean useState showWarning

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -3,7 +3,6 @@ import Warning from "./Warning";
 
 export default function Textarea() {
     const [text, setText] = useState('');
-    const [showWarning, setShowWarning] = useState(false);
     const [warningText, setWarningText] = useState('');
 
     const handleChange = (e) => {
@@ -11,15 +10,12 @@ export default function Textarea() {
 
         if (newText.includes('<script>')) {
             setWarningText('No script tag allowed!')
-            setShowWarning(true);
             newText = newText.replace('<script>', '');
         } else if (newText.includes('<?php')) {
             setWarningText('No php tag allowed!')
-            setShowWarning(true);
             newText = newText.replace('<?php', '');
         } else if (newText.includes('<html>')) {
             setWarningText('No html tag allowed!')
-            setShowWarning(true);
             newText = newText.replace('<html>', '');
         }
         setText(newText);
@@ -28,7 +24,7 @@ export default function Textarea() {
         <div className="textarea">
             <textarea value={text} onChange={handleChange} placeholder="Enter your text here..." spellCheck="false" />
             {
-                showWarning ? <Warning warningText={warningText} /> : null
+                warningText ? <Warning warningText={warningText} /> : null
             }
         </div>
     );
